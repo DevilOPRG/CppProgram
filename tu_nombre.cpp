3f7b53632584929c7a164a0247f1021465e3459c
DevilOPRG <oskar.pablo@gmail.com>
2019-10-02 01:07:49 -0500

    New Cpp file added

---
+++ tu_nombre.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+#include "conio.h"
+#include "stdlib.h"
+#include "string"
+#include "cstdlib"
+#include "stdio.h"
+
+using namespace std;
+main()
+{
+string x, y, z, a;
+	cout<<"Ingresa tu nombre"<<endl;
+	cin>>x>>y>>z>>a;
+	cout<<"Hola"<<endl<<x<<" "<<y<<" "<<z<<" "<<a<<endl;
+	getch();
+	
+}
+
